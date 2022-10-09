18e0e7d910eb5797b3b200a54a19aeca2ae85410
latypovroman <jiggawhat5@gmail.com>
2022-10-09 16:34:33 +0500

    started project, added template and start list

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import TodoList from "./components/TodoList.vue";
+
+export default {
+  components: {TodoList},
+}
+</script>
+
+<template>
+
+  <h1 class="header">Список дел</h1>
+  <TodoList />
+
+</template>
+
+<style>
+
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul>
+    <li v-for="item in todo" :key="item.id">
+      {{item.title}}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "TodoList",
+  data() {
+    return {
+      todo: [
+        { id: 1, title: "Посмотреть лекции" },
+        { id: 2, title: "Разобраться с vue" },
+        { id: 3, title: "Запилить приложение" }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
